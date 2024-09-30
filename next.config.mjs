16ff1dcaf68a859204242b1210dc6fab3362338d
Harry Bradshaw <harry.bradshaw@softwire.com>
2024-09-30 22:23:30 +0100

    Try no RSC cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-      },
-    ],
-  },
-  logging: {
-    fetches: {
-      fullUrl: true,
-      hmrRefreshes: true,
-
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.ctfassets.net",
+            },
+        ],
     },
-  },
+    logging: {
+        fetches: {
+            fullUrl: true,
+            hmrRefreshes: true,
+        },
+    },
+    experimental: {
+        staleTimes: {
+            dynamic: 0,
+            static: 0,
+        },
+    },
 };
 
 export default nextConfig;
